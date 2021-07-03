d5ca28d6e910f7ebf7c209c9b51c2b44fb216e6c
Mars.P <marsxxd@outlook.com>
2021-07-04 00:49:41 +0800

    Add test

---
+++ src/SugarTalk.Tests/TestBase.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SugarTalk.Core;
+
+namespace SugarTalk.Tests
+{
+    public class TestBase : IDisposable
+    {
+        private readonly IConfigurationRoot _configuration;
+        private readonly IServiceCollection _serviceCollection;
+        
+        public TestBase()
+        {
+            _configuration = LoadConfiguration();
+            _serviceCollection = RegisterServiceCollection();
+        }
+
+        private IConfigurationRoot LoadConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+        
+        private IServiceCollection RegisterServiceCollection()
+        {
+            return new ServiceCollection()
+                .LoadSugarTalkModule()
+                .LoadSettings(_configuration)
+                .LoadMongoDb()
+                .LoadMediator();
+        }
+
+        protected async Task Run<T>(Func<T, Task> action)
+        {
+            await action(_serviceCollection.BuildServiceProvider().GetService<T>());
+        }
+        
+        public void Dispose()
+        {
+        }
+    }
+}
