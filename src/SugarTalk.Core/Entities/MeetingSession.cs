d6d7d03aa347eb02546b66e0173cbc2f8031e22f
Mars.P <marsxxd@outlook.com>
2021-08-07 15:17:45 +0800

    remove comment code

--- src/SugarTalk.Core/Entities/MeetingSession.cs
+++ src/SugarTalk.Core/Entities/MeetingSession.cs
@@ -10,34 +10,5 @@ namespace SugarTalk.Core.Entities
         public string MeetingNumber { set; get; }
         public MeetingType MeetingType { get; set; }
         public string PipelineId { get; set; }
-        
-        // public async Task RemoveAsync(string id)
-        // {
-        //     if (UserSessions.TryRemove(id, out UserSession user))
-        //     {
-        //         //释放自身资源
-        //         if (user.SendEndPoint != null)
-        //         {
-        //             await user.SendEndPoint.ReleaseAsync();
-        //         }
-        //         if (user.ReceivedEndPoints != null)
-        //         {
-        //             foreach (var endPoint in user.ReceivedEndPoints.Values)
-        //             {
-        //                 await endPoint.ReleaseAsync();
-        //             }
-        //         }
-        //         //释放其他人员的资源
-        //         foreach (var u in UserSessions.Values)
-        //         {
-        //             if (u.ReceivedEndPoints.TryRemove(id, out WebRtcEndpoint endpoint))
-        //             {
-        //                 await endpoint.ReleaseAsync();
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // public IEnumerable<UserSession> GetOtherUsers(string connectionId) => UserSessions.Values.Where(x => x.Id != connectionId);
     }
 }
