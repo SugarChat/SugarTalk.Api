bd925144c0f7eb33b74bb7aa0ce3c845f1fd5275
Mars.P <marsxxd@outlook.com>
2021-08-24 23:08:48 +0800

    enhance

--- src/SugarTalk.Core/Settings/WebRtcIceServerSettings.cs
+++ src/SugarTalk.Core/Settings/WebRtcIceServerSettings.cs
@@ -9,7 +9,7 @@ namespace SugarTalk.Core.Settings
     {
         public string[] Urls { get; set; }
         
-        public string UserName { get; set; }
+        public string Username { get; set; }
         
         public string Credential { get; set; }
         
