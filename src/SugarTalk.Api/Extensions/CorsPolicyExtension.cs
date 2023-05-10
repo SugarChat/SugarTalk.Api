13eceace6d59b898ae54a4b26d376fac905b0e90
Liu <1304055639@qq.com>
2023-05-10 17:30:47 +0800

    add cors

---
+++ src/SugarTalk.Api/Extensions/CorsPolicyExtension.cs
@@ -0,0 +1,22 @@
+using SugarTalk.Core.Settings.CorsPolicy;
+
+namespace SugarTalk.Api.Extensions;
+
+public static class CorsPolicyExtension
+{
+    public static IServiceCollection AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                {
+                    policy.WithOrigins(new AllowableCorsOriginsSetting(configuration).Value)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+        });
+        return services;
+    }
+}
---
+++ src/SugarTalk.Core/Settings/CorsPolicy/AllowableCorsOriginsSetting.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SugarTalk.Core.Settings.CorsPolicy
+{
+    public class AllowableCorsOriginsSetting : IConfigurationSetting<string[]>
+    {
+        public AllowableCorsOriginsSetting(IConfiguration configuration)
+        {
+            Value = configuration.GetValue<string>("AllowableCorsOrigins").Split(',');
+        }
+
+        public string[] Value { get; set; }
+    }
+}
