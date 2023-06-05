d10cc58cbb5a9dec60232e2d464e5623235bd145
Liu <1304055639@qq.com>
2023-06-05 09:08:03 +0800

    update format file

--- src/SugarTalk.Core/Domain/Meeting/MeetingUserSessionStream.cs
+++ src/SugarTalk.Core/Domain/Meeting/MeetingUserSessionStream.cs
@@ -11,13 +11,13 @@ public class MeetingUserSessionStream : IEntity
     [Column("id")]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
     [Column("meeting_user_session_id")]
     public int MeetingUserSessionId { get; set; }
-    
+
     [Column("stream_id"), StringLength(128)]
     public string StreamId { get; set; }
-    
+
     [Column("stream_type")]
     public MeetingStreamType StreamType { get; set; }
 }
