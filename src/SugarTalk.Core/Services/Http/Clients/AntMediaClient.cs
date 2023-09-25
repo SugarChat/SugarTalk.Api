d21f2e6a5a45f699b74152aee09b76a711188df5
Liu <1304055639@qq.com>
2023-09-25 19:21:01 +0800

    Update AntMediaClient.cs

--- src/SugarTalk.Core/Services/Http/Clients/AntMediaClient.cs
+++ src/SugarTalk.Core/Services/Http/Clients/AntMediaClient.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Serilog;
 using SugarTalk.Core.Ioc;
 using SugarTalk.Core.Settings.AntMedia;
 using SugarTalk.Messages.Dto.AntMedia;
@@ -46,56 +47,84 @@ public class AntMediaServerClient : IAntMediaServerClient
     public async Task<GetMeetingResponseDto> GetConferenceRoomAsync(
         string appName, string meetingNumber, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .GetAsync<GetMeetingResponseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/{meetingNumber}", cancellationToken).ConfigureAwait(false);
+
+        Log.Information("Ant Media to get conference room, response: {response}", response);
+        
+        return response;
     }
 
     public async Task<List<ConferenceRoomDto>> GetConferenceRoomsAsync(
         string appName, int offset, int size, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .GetAsync<List<ConferenceRoomDto>>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/list/{offset}/{size}", cancellationToken).ConfigureAwait(false);
+        
+        Log.Information("Ant Media to get all conference room, response: {response}", response);
+
+        return response;
     }
 
     public async Task<GetConferenceRoomInfoResponseDto> GetConferenceRoomInfoAsync(
         string appName, string roomId, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .GetAsync<GetConferenceRoomInfoResponseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/{roomId}/room-info", cancellationToken).ConfigureAwait(false);
+    
+        Log.Information("Ant Media to get conference room info, response: {response}", response);
+        
+        return response;
     }
 
     public async Task<CreateMeetingResponseDto> CreateConferenceRoomAsync(
         string appName, CreateMeetingDto meetingData, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .PostAsJsonAsync<CreateMeetingResponseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms", meetingData, cancellationToken).ConfigureAwait(false);
+        
+        Log.Information("Ant Media to create conference room, response: {response}", response);
+        
+        return response;
     }
 
     public async Task<ConferenceRoomResponseBaseDto> DeleteConferenceRoomAsync(
         string appName, string meetingNumber, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .DeleteAsync<ConferenceRoomResponseBaseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/{meetingNumber}", cancellationToken).ConfigureAwait(false);
+        
+        Log.Information("Ant Media to delete conference room, response: {response}", response);
+        
+        return response;
     }
 
     public async Task<ConferenceRoomResponseBaseDto> AddStreamToConferenceRoomAsync(
         string appName, string meetingNumber, string streamId, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .PutAsync<ConferenceRoomResponseBaseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/{meetingNumber}/add?streamId={streamId}", null, cancellationToken).ConfigureAwait(false);
+        
+        Log.Information("Ant Media add stream to conference room , response: {response}", response);
+        
+        return response;
     }
     
     public async Task<ConferenceRoomResponseBaseDto> DeleteStreamFromConferenceRoomAsync(
         string appName, string meetingNumber, string streamId, CancellationToken cancellationToken)
     {
-        return await _httpClientFactory
+        var response = await _httpClientFactory
             .PutAsync<ConferenceRoomResponseBaseDto>(
                 $"{_antMediaSetting.BaseUrl}/{appName}/rest/v2/broadcasts/conference-rooms/{meetingNumber}/delete?streamId={streamId}", null, cancellationToken).ConfigureAwait(false);
+        
+        Log.Information("Ant Media delete stream from conference room, response: {response}", response);
+        
+        return response;
     }
 }
