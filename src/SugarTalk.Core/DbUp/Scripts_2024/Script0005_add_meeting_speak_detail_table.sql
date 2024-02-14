996ec1fb9ba98524fe60c888391dff6861d47f9d
MonesyH <monesy.huang@sjfood.com>
2024-02-14 09:35:13 +0800

    Update

--- src/SugarTalk.Core/DbUp/Scripts_2024/Script0005_add_meeting_speak_detail_table.sql
+++ src/SugarTalk.Core/DbUp/Scripts_2024/Script0005_add_meeting_speak_detail_table.sql
@@ -1,10 +1,7 @@
 create table if not exists `meeting_speak_detail`
 (
     `id` varchar(36) not null primary key,
-    `meeting_id` varchar(36) not null,
-    `meeting_sub_id` varchar(36) null,
     `meeting_record_id` varchar(36) not null,
-    `meeting_number` varchar(128) not null,
     `user_id` int not null,
     `speak_start_time` BIGINT not null,
     `speak_end_time` BIGINT null,
@@ -14,6 +11,4 @@ create table if not exists `meeting_speak_detail`
 ) charset=utf8mb4;
 
 CREATE INDEX idx_user_id ON meeting_speak_detail (user_id);
-CREATE INDEX idx_meeting_id ON meeting_speak_detail (meeting_id);
-CREATE INDEX idx_meeting_sub_id ON meeting_speak_detail (meeting_sub_id);
 CREATE INDEX idx_meeting_record_id ON meeting_speak_detail (meeting_record_id);
--- src/SugarTalk.Core/Domain/Meeting/MeetingSpeakDetail.cs
+++ src/SugarTalk.Core/Domain/Meeting/MeetingSpeakDetail.cs
@@ -12,17 +12,8 @@ public class MeetingSpeakDetail : IEntity
     [Column("id", TypeName = "char(36)")]
     public Guid Id { get; set; }
     
-    [Column("meeting_id", TypeName = "char(36)")]
-    public Guid MeetingId { get; set; }
-    
-    [Column("meeting_sub_id", TypeName = "char(36)")]
-    public Guid? MeetingSubId { get; set; }
-    
     [Column("meeting_record_id", TypeName = "char(36)")]
     public Guid MeetingRecordId { get; set; }
-    
-    [Column("meeting_number"), StringLength(128)]
-    public string MeetingNumber { get; set; }
     
     [Column("user_id")]
     public int UserId { get; set; }
