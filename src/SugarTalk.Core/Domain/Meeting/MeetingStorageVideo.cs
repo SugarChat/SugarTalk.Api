72695da2eaa665d8b6d43adf6f0def4536f54b2f
ZEKE.Z <zeke.zuo@sjfood.com>
2024-02-15 12:58:32 +0800

    start meeting store video

---
+++ src/SugarTalk.Core/Domain/Meeting/MeetingStorageVideo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarTalk.Core.Domain.Meeting
+{
+    public class MeetingStorageVideo : IEntity
+    {
+
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("meeting_id", TypeName = "char(36)")]
+        public Guid MeetingId { get; set; }
+
+        /// <summary>
+        /// 会议号
+        /// </summary>
+        [Column("meeting_number"), StringLength(256)]
+        public string MeetingNumber { get; set; }
+
+        /// <summary>
+        /// 会议标题
+        /// </summary>
+        [Column("meeting_title")]
+        public string MeetingTitle { get; set; }
+
+        /// <summary>
+        /// 开始日期
+        /// </summary>
+        [Column("start_date")]
+        public long StartDate { get; set; }
+
+        /// <summary>
+        /// 结束日期
+        /// </summary>
+        [Column("end_date")]
+        public long EndDate { get; set; }
+
+        /// <summary>
+        /// 视频Url
+        /// </summary>
+        [Column("video_url")]
+        public string VideoUrl { get; set; }
+    }
+}
---
+++ src/SugarTalk.Messages/Dto/Meetings/StorageMeetingVideoDto.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarTalk.Messages.Dto.Meetings
+{
+    public class StorageMeetingVideoDto
+    {
+        [JsonProperty("meetingTitle")]
+        public string MeetingTitle { get; set; }
+
+        [JsonProperty("startDate")]
+        public long StartDate { get; set; }
+
+        [JsonProperty("endDate")]
+        public long EndDate { get; set; }
+
+        [JsonProperty("meetingNumber")]
+        public string MeetingNumber { get; set; }
+
+        /// <summary>
+        /// 记录编号
+        /// </summary>
+        [JsonProperty("recordNumber")]
+        public string RecordNumber { get; set; }
+
+        [JsonProperty("viderUrl")]
+        public string VideoUrl { get; set; }
+    }
+}
