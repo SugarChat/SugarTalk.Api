cc063ffa5949ccd9c905e5c8d967af16068a264e
JERICHO.Z <547243012@qq.com>
2024-02-17 13:12:25 +0800

    guest join meeting

--- src/SugarTalk.Api/Authentication/Guest/GuestAuthenticationHandler.cs
+++ src/SugarTalk.Api/Authentication/Guest/GuestAuthenticationHandler.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using SugarTalk.Core.Constants;
-using SugarTalk.Core.Data;
 using SugarTalk.Core.Data.Claims;
-using SugarTalk.Core.Domain.Account;
-using SugarTalk.Core.Extensions;
+using SugarTalk.Core.Services.Account;
 using SugarTalk.Messages.Enums.Account;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
@@ -13,46 +11,38 @@ namespace SugarTalk.Api.Authentication.Guest
 {
     public class GuestAuthenticationHandler : AuthenticationHandler<GuestAuthenticationOptions>
     {
-        private readonly SugarTalkDbContext _dbContext;
+        private readonly IAccountDataProvider _accountDataProvider;
 
-        public GuestAuthenticationHandler(IOptionsMonitor<GuestAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, SugarTalkDbContext dbContext)
+        public GuestAuthenticationHandler(IOptionsMonitor<GuestAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IAccountDataProvider accountDataProvider)
             : base(options, logger, encoder, clock)
         {
-            _dbContext = dbContext;
+            _accountDataProvider = accountDataProvider;
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
+            if (!Request.Headers.ContainsKey(RequestHeaderKeys.Issuer))
+                return AuthenticateResult.NoResult();
+
             var issuser = Request.Headers[RequestHeaderKeys.Issuer].ToString();
             if (!string.IsNullOrWhiteSpace(issuser))
             {
-                if (int.TryParse(issuser, out var issuserInt))
+                if (int.TryParse(issuser, out var issuserInt) && (UserAccountIssuer)issuserInt == UserAccountIssuer.Guest)
                 {
-                    if ((UserAccountIssuer)issuserInt == UserAccountIssuer.Guest)
+                    var userName = Request.Headers.ContainsKey(RequestHeaderKeys.GuestSessionId) ? Request.Headers[RequestHeaderKeys.GuestSessionId].ToString() : Guid.NewGuid().ToString();
+                    var account = await _accountDataProvider.CreateUserAccountAsync(userName,
+                        string.Empty,
+                        authType: UserAccountIssuer.Guest).ConfigureAwait(false);
+
+                    var identity = new ClaimsIdentity(new[]
                     {
-                        var userAccount = new UserAccount
-                        {
-                            CreatedOn = DateTime.Now,
-                            ModifiedOn = DateTime.Now,
-                            Uuid = Guid.NewGuid(),
-                            UserName = Guid.NewGuid().ToString(),
-                            Password = string.Empty.ToSha256(),
-                            ThirdPartyUserId = Guid.NewGuid().ToString(),
-                            Issuer = UserAccountIssuer.Guest,
-                            IsActive = true
-                        };
-                        await _dbContext.AddAsync(userAccount);
-                        await _dbContext.SaveChangesAsync();
-                        var identity = new ClaimsIdentity(new[]
-                        {
-                            new Claim(ClaimTypes.Name, userAccount.UserName),
-                            new Claim(ClaimTypes.NameIdentifier, userAccount.Id.ToString()),
-                            new Claim(ClaimTypes.Authentication, UserAccountIssuer.Wiltechs.ToString())
-                        }, AuthenticationSchemeConstants.GuestAuthenticationScheme);
-                        var claimPrincipal = new ClaimsPrincipal(identity);
-                        var ticket = new AuthenticationTicket(claimPrincipal, new AuthenticationProperties { IsPersistent = false }, Scheme.Name);
-                        return AuthenticateResult.Success(ticket);
-                    }
+                        new Claim(ClaimTypes.Name, account.UserName),
+                        new Claim(ClaimTypes.NameIdentifier, account.Id.ToString()),
+                        new Claim(ClaimTypes.Authentication, UserAccountIssuer.Guest.ToString())
+                    }, AuthenticationSchemeConstants.GuestAuthenticationScheme);
+                    var claimPrincipal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(claimPrincipal, new AuthenticationProperties { IsPersistent = false }, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
                 }
             }
 
--- src/SugarTalk.Core/Data/Claims/RequestHeaderKeys.cs
+++ src/SugarTalk.Core/Data/Claims/RequestHeaderKeys.cs
@@ -3,5 +3,6 @@
     public static class RequestHeaderKeys
     {
         public const string Issuer = "issuer";
+        public const string GuestSessionId = "X-Guest-Session-Id";
     }
 }
