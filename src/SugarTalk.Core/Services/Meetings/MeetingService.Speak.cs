911ff0933c6e90ef408125f548aa01d5e50eb096
FREYA.F <1196094290@qq.com>
2024-02-26 17:08:05 +0800

    update

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Speak.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Speak.cs
@@ -71,14 +71,14 @@ public partial class MeetingService
 
         meetingSpeakDetail.FileTranscriptionStatus = egressItem.Status switch
         {
-            "EGRESS_COMPLETE" or "EGRESS_LIMIT_REACHED" when !string.IsNullOrEmpty(egressItem.File.Location) => FileTranscriptionStatus.InProcess,
-            "EGRESS_STARTING" or "EGRESS_ENDING" when meetingSpeakDetail.FileTranscriptionStatus == FileTranscriptionStatus.Pending => FileTranscriptionStatus.InProcess,
+            "EGRESS_COMPLETE" or "EGRESS_LIMIT_REACHED" when !string.IsNullOrEmpty(egressItem.File.Location) => FileTranscriptionStatus.InProcess, 
+            "EGRESS_ENDING" or "EGRESS_STARTING" => meetingSpeakDetail.FileTranscriptionStatus,
             _ => FileTranscriptionStatus.Exception
         };
 
         await _meetingDataProvider.UpdateMeetingSpeakDetailAsync(meetingSpeakDetail, cancellationToken: cancellationToken).ConfigureAwait(false);
 
-        if (meetingSpeakDetail.FileTranscriptionStatus == FileTranscriptionStatus.InProcess)
+        if ((egressItem.Status == "EGRESS_COMPLETE" || egressItem.Status == "EGRESS_LIMIT_REACHED") && meetingSpeakDetail.FileTranscriptionStatus == FileTranscriptionStatus.InProcess)
         {
             _backgroundJobClient.Enqueue(() => TranscriptionMeetingAsync(meetingSpeakDetail, cancellationToken));
         }
