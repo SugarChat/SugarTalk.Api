3333b237a942dcad7254e7837ad9a3282f6117a1
FREYA.F <1196094290@qq.com>
2024-02-26 16:28:43 +0800

    format

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Speak.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Speak.cs
@@ -71,7 +71,7 @@ public partial class MeetingService
 
         meetingSpeakDetail.FileTranscriptionStatus = egressItem.Status switch
         {
-            "EGRESS_COMPLETE" or "EGRESS_LIMIT_REACHED" when !string.IsNullOrEmpty(egressItem.File.Location)=> FileTranscriptionStatus.InProcess,
+            "EGRESS_COMPLETE" or "EGRESS_LIMIT_REACHED" when !string.IsNullOrEmpty(egressItem.File.Location) => FileTranscriptionStatus.InProcess,
             "EGRESS_STARTING" or "EGRESS_ENDING" when meetingSpeakDetail.FileTranscriptionStatus == FileTranscriptionStatus.Pending => FileTranscriptionStatus.InProcess,
             _ => FileTranscriptionStatus.Exception
         };
