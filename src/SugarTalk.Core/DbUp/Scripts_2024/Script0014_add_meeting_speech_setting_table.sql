fb3a3c0a42589144c9c440acae5b36bcd17ec5fe
JOHNNY.X <2546088435@qq.com>
2024-03-05 14:33:31 +0800

    update

---
+++ src/SugarTalk.Core/DbUp/Scripts_2024/Script0014_add_meeting_speech_setting_table.sql
@@ -0,0 +1,15 @@
+create table if not exists `meeting_speech_setting`
+(
+    `id` int primary key auto_increment,
+    `voice_name` varchar(50) not null,
+    `created_date` datetime(3) not null
+)charset=utf8mb4;
+
+create table if not exists `meeting_voice_tone_chart`
+(
+    `id` int primary key auto_increment,
+    `setting_id` int not null,
+    `voice_id` varchar(48) not null,
+    `language_id` varchar(48) not null,
+    `created_date` datetime(3) not null
+)
