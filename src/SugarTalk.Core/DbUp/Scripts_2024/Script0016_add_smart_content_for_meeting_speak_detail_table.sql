5056272e9e2801b58759f549f502e66e15539f77
LIZZIE.L <lizzie.li@sjfood.com>
2024-03-09 09:21:12 +0800

    make original content null

--- src/SugarTalk.Core/DbUp/Scripts_2024/Script0016_add_smart_content_for_meeting_speak_detail_table.sql
+++ src/SugarTalk.Core/DbUp/Scripts_2024/Script0016_add_smart_content_for_meeting_speak_detail_table.sql
@@ -2,4 +2,4 @@ ALTER TABLE meeting_speak_detail
     ADD smart_content TEXT NULL;
 
 ALTER TABLE meeting_speak_detail
-    CHANGE COLUMN speak_content original_content TEXT;
+    CHANGE COLUMN speak_content original_content TEXT NULL;
