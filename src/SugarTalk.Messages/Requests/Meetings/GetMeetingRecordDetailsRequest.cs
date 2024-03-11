b6627cdd557a8df5148b08f20ee25b20512b96f3
JOHNNY.X <2546088435@qq.com>
2024-03-11 16:50:43 +0800

    get meeting speak detail

--- src/SugarTalk.Messages/Requests/Meetings/GetMeetingRecordDetailsRequest.cs
+++ src/SugarTalk.Messages/Requests/Meetings/GetMeetingRecordDetailsRequest.cs
@@ -1,6 +1,7 @@
 using System;
 using Mediator.Net.Contracts;
 using SugarTalk.Messages.Dto.Meetings;
+using SugarTalk.Messages.Dto.Translation;
 using SugarTalk.Messages.Responses;
 
 namespace SugarTalk.Messages.Requests.Meetings;
@@ -8,6 +9,8 @@ namespace SugarTalk.Messages.Requests.Meetings;
 public class GetMeetingRecordDetailsRequest : IRequest
 {
     public Guid Id { get; set; }
+
+    public TranslationLanguage Language { get; set; }
 }
 
 public class GetMeetingRecordDetailsResponse : SugarTalkResponse<GetMeetingRecordDetailsDto>
