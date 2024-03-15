c58c23f6e9ce912db71ea49cd33320b2d8161be3
LIZZIE.L <lizzie.li@sjfood.com>
2024-03-15 15:54:42 +0800

    Add chat room voice record table

---
+++ src/SugarTalk.Core/DbUp/Scripts_2024/Script0019_add_meeting_chat_voice_record_table.sql
@@ -0,0 +1,9 @@
+create table if not exists `meeting_chat_voice_record`(
+    id varchar(36) not null primary key,
+    voice_language int not null,
+    created_date datetime(3) not null,
+    speech_id varchar(36) not null,
+    voice_id varchar(36) not null,
+    is_system_voice tinyint(1) not null default 0,
+    voice_url varchar(512) null
+) charset=utf8mb4;
---
+++ src/SugarTalk.Core/Domain/Meeting/MeetingChatVoiceRecord.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SugarTalk.Messages.Enums.Speech;
+
+namespace SugarTalk.Core.Domain.Meeting;
+
+[Table("meeting_chat_voice_record")]
+public class MeetingChatVoiceRecord : IEntity
+{
+    public MeetingChatVoiceRecord()
+    {
+        CreatedDate = DateTimeOffset.Now;
+    }
+    
+    [Key]
+    [Column("id", TypeName = "char(36)")]
+    public Guid Id { get; set; }
+
+    [Column("voice_language")]
+    public SpeechTargetLanguageType VoiceLanguage { get; set; }
+    
+    [Column("created_date")]
+    public DateTimeOffset CreatedDate { get; set; }
+    
+    [Column("speech_id")]
+    public Guid SpeechId { get; set; }
+    
+    [Column("voice_id")]
+    public string VoiceId { get; set; }
+    
+    [Column("is_system_voice", TypeName = "tinyint(1)")]
+    public bool IsSystemVoice { get; set; }
+    
+    [Column("voice_url")]
+    public string VoiceUrl { get; set; }
+}
