31c7be5371435e90c9fe6d268f35ef28e30457bc
LIZZIE.L <lizzie.li@sjfood.com>
2024-03-28 10:05:10 +0800

    Add speech interface

---
+++ src/SugarTalk.Messages/Dto/Meetings/Speech/SpeechInferenceDto.cs
@@ -0,0 +1,45 @@
+using Newtonsoft.Json;
+
+namespace SugarTalk.Messages.Dto.Meetings.Speech;
+
+public class SpeechInferenceDto
+{
+    [JsonProperty("name")] 
+    public string Name { get; set; }
+    
+    [JsonProperty("language_id")]
+    public int LanguageId { get; set; }
+    
+    [JsonProperty("text")]
+    public string Text { get; set; }
+    
+    [JsonProperty("transpose")]
+    public int Transpose { get; set; }
+    
+    [JsonProperty("speed")]
+    public int Speed { get; set; }
+    
+    [JsonProperty("response_format")]
+    public string ResponseFormat { get; set; }
+}
+
+public class SpeechInferenceResponseDto
+{
+    [JsonProperty("status")]
+    public int Status { get; set; }
+
+    [JsonProperty("message")]
+    public string Message { get; set; }
+
+    [JsonProperty("result")]
+    public SpeechInferenceResultDto Result { get; set; }
+}
+
+public class SpeechInferenceResultDto
+{
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("base64")]
+    public string Base64 { get; set; }
+}
