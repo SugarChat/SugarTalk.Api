18a29b1bfc7a812459d718ad7133c52b89f5fb83
JOHNNY.X <2546088435@qq.com>
2024-03-28 09:56:57 +0800

    update

--- src/SugarTalk.Core/Services/LiveKit/LivekitServerUtilService.cs
+++ src/SugarTalk.Core/Services/LiveKit/LivekitServerUtilService.cs
@@ -73,7 +73,7 @@ public class LiveKitServerUtilService : ILiveKitServerUtilService
         Log.Information("GenerateTokenForJoinMeeting: meetingNumber: {meetingNumber}, userId: {deviceId}, username: {username}", meetingNumber, user.Id, user.UserName);
         
         return generateAccessToken.JoinMeeting(
-            meetingNumber, _liveKitServerSetting.Apikey, _liveKitServerSetting.ApiSecret, user.Id.ToString(), user.UserName);
+            meetingNumber, _liveKitServerSetting.Apikey, _liveKitServerSetting.ApiSecret, user.UserName, user.UserName);
     }
 
     public string GenerateTokenForRecordMeeting(UserAccountDto user, string meetingNumber)
