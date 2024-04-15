dd79758c6ebf3c9d01a91eb6f63ac88ba20157ed
LIZZIE.L <lizzie.li@sjfood.com>
2024-04-15 10:24:53 +0800

    format

--- src/SugarTalk.Messages/Commands/Meetings/Speak/AddOrUpdateChatRoomSettingCommand.cs
+++ src/SugarTalk.Messages/Commands/Meetings/Speak/AddOrUpdateChatRoomSettingCommand.cs
@@ -19,9 +19,9 @@ public class AddOrUpdateChatRoomSettingCommand : ICommand
     
     public int Transpose { get; set; }
     
-    public SpeechTargetLanguageType ListeningLanguage { get; set; }
-    
     public SpeechTargetLanguageType SelfLanguage { get; set; }
+    
+    public SpeechTargetLanguageType ListeningLanguage { get; set; }
 }
 
 public class AddOrUpdateChatRoomSettingResponse : SugarTalkResponse
