baed2feb6acf468625da26638ecc9ff3842b04e1
JOHNNY.X <2546088435@qq.com>
2024-05-07 08:41:37 +0800

    Update BaseFixture.cs

--- src/SugarTalk.UnitTests/BaseFixture.cs
+++ src/SugarTalk.UnitTests/BaseFixture.cs
@@ -1,3 +1,4 @@
+using Aliyun.OSS;
 using AutoMapper;
 using NSubstitute;
 using SugarTalk.Core.Data;
@@ -68,7 +69,7 @@ public partial class BaseFixture
         _sugarTalkBackgroundJobClient = Substitute.For<ISugarTalkBackgroundJobClient>();
         _accountDataProvider = MockAccountDataProvider(_mapper, _repository, _unitOfWork, _currentUser);
         _meetingDataProvider = MockMeetingDataProvider(_clock, _mapper, _repository, _unitOfWork, _currentUser, _accountDataProvider);
-        _aliYunOssService = Substitute.For<IAliYunOssService>();
+        _aliYunOssService = MockAliYunOssService(Substitute.For<OssClient>(), Substitute.For<AliYunOssSettings>());
         _meetingService = MockMeetingService(_clock, _mapper, _unitOfWork, _currentUser, _openAiService, _speechClient,
             _liveKitClient, _ffmpegService, _aliYunOssSetting, _translationClient, _aliYunOssService, _meetingUtilService, _meetingDataProvider, _accountDataProvider, _liveKitServerSetting,
             _httpClientFactory, _backgroundJobClient, _liveKitServerUtilService, _antMediaServerUtilService, _sugarTalkBackgroundJobClient); 
@@ -124,7 +125,14 @@ public partial class BaseFixture
     {
         return new MeetingDataProvider(clock, mapper, repository, unitOfWork, currentUser, accountDataProvider);
     }
-    
+
+    protected IAliYunOssService MockAliYunOssService(
+        OssClient ossClient,
+        AliYunOssSettings ossSettings)
+    {
+        return new AliYunOssService(ossClient, ossSettings);
+    }
+
     protected IMapper CreateMapper()
     {
         var config = new MapperConfiguration(cfg =>
