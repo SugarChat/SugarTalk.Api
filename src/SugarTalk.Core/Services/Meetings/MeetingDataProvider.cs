1365f488d7d2ac7d0c2a7c217456760477906fa2
JOHNNY.X <2546088435@qq.com>
2024-05-20 14:09:02 +0800

    Update MeetingDataProvider.cs

--- src/SugarTalk.Core/Services/Meetings/MeetingDataProvider.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingDataProvider.cs
@@ -499,7 +499,7 @@ namespace SugarTalk.Core.Services.Meetings
         public async Task<(int Count, List<AppointmentMeetingDto> Records)> GetAppointmentMeetingsByUserIdAsync(GetAppointmentMeetingsRequest request, CancellationToken cancellationToken)
         {
             var maxQueryDate = _clock.Now.AddMonths(1).ToUnixTimeSeconds();
-            var startOfDay = new DateTimeOffset(_clock.Now.Year, _clock.Now.Month, _clock.Now.Day, 0, 0, 0, TimeSpan.Zero).ToUnixTimeSeconds();
+            var startOfDay = new DateTimeOffset(_clock.Now.Year, _clock.Now.Month, _clock.Now.Day, 0, 0, 0, TimeSpan.FromHours(8)).ToUnixTimeSeconds();
             
             Log.Information("GetAppointmentMeetingsByUserIdAsync maxQueryDate:{@maxQueryDate},startOfDay:{@startofDay}", maxQueryDate, startOfDay);
 
