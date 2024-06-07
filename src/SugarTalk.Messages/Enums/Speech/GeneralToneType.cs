261ad8f12a5faddef6f7cb912331e0bdc4704c48
LIZZIE.L <1339611863@qq.com>
2024-06-07 14:42:33 +0800

    Fix meeting auto distribute voice id

---
+++ src/SugarTalk.Messages/Enums/Speech/GeneralToneType.cs
@@ -0,0 +1,16 @@
+namespace SugarTalk.Messages.Enums.Speech;
+
+public enum GeneralToneType
+{
+    Neural1 = 201,
+    
+    Neural2 = 202,
+    
+    Neural3 = 203,
+    
+    Neural4 = 204,
+    
+    Neural5 = 205,
+    
+    Neural6 = 206,
+}
