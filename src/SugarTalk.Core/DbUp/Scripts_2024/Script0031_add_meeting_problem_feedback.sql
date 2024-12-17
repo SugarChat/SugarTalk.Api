416dcb25c0ad5861c49ce03fe841cc49cd88e62a
157 <serena.xiao@sjfood.com>
2024-12-17 09:45:14 +0800

    fix table

--- src/SugarTalk.Core/DbUp/Scripts_2024/Script0031_add_meeting_problem_feedback.sql
+++ src/SugarTalk.Core/DbUp/Scripts_2024/Script0031_add_meeting_problem_feedback.sql
@@ -3,12 +3,9 @@ create table if not exists `meeting_problem_feedback`
     `id` INT NOT NULL AUTO_INCREMENT,
     `category` INT NOT NULL,
     `description` TEXT NOT NULL,
-    `is_new` BIT NOT NULL,
     `created_date` datetime(3) not null,
     `created_by` int not null,
     `last_modified_by` int not null,
     `last_modified_date` datetime(3) not null,
     PRIMARY KEY(`id`)
     )charset=utf8mb4;
-
-ALTER TABLE meeting_problem_feedback DROP COLUMN `is_new`;
