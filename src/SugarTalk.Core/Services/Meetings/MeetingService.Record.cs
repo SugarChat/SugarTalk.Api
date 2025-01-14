71c47cc6d9f253e445184c5f3093bc5f2f13ebb1
JOHNNY.X <2546088435@qq.com>
2025-01-14 17:11:55 +0800

    update

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
@@ -476,12 +476,10 @@ public partial class MeetingService
     private async Task<string> ConvertPdfAsync(string content, Guid summaryId, TranslationLanguage targetLanguage, PdfExportType pdfExportType, CancellationToken cancellationToken)
     {
         var fileName = $"Sugartalk/{Guid.NewGuid()}.pdf";
-        
-        using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(_asposeTotalNetSettings.AsposeTotalNetContent)))
-        {
-            var license = new Aspose.Words.License();
-            license.SetLicense(stream);
-       
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(_asposeTotalNetSettings.AsposeTotalNetContent));
+        var license = new Aspose.Words.License();
+        license.SetLicense(stream);
         
         var pdfDocument = new Document();
         var page = pdfDocument.Pages.Add();
@@ -492,7 +490,7 @@ public partial class MeetingService
         page.Paragraphs.Add(textFragment);
 
         using var memoryStream = new MemoryStream();
-        pdfDocument.Save(memoryStream);
+        await pdfDocument.SaveAsync(memoryStream, cancellationToken).ConfigureAwait(false);
         memoryStream.Position = 0;
         
         await _awsS3Service.UploadFileAsync(fileName: fileName, fileContent: memoryStream.ToArray(), cancellationToken: cancellationToken).ConfigureAwait(false);
@@ -504,8 +502,7 @@ public partial class MeetingService
             PdfExportType = pdfExportType,
             TargetLanguage = targetLanguage
         }, cancellationToken: cancellationToken).ConfigureAwait(false);
-        }
-        
+
         return fileName;
     }
 
