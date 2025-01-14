52be6a9e4e5c6fd37f70d3ef265bcd163d2e425c
JOHNNY.X <2546088435@qq.com>
2025-01-14 17:43:14 +0800

    Update MeetingService.Record.cs

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
@@ -476,7 +476,7 @@ public partial class MeetingService
     private async Task<string> ConvertPdfAsync(string content, Guid summaryId, TranslationLanguage targetLanguage, PdfExportType pdfExportType, CancellationToken cancellationToken)
     {
         var license = new Aspose.Words.License();
-        license.SetLicense("Aspose.Total.NET.txt");
+        license.SetLicense("src/SugarTalk.Core/Services/Aspose.Total.NET.txt");
         
         var pdfDocument = new Document();
         var page = pdfDocument.Pages.Add();
