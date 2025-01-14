568dbb3967f85e72dbe546324dcf65fbaf8d67aa
157 <serena.xiao@sjfood.com>
2025-01-14 09:20:36 +0800

    add get interface

---
+++ src/SugarTalk.Messages/Requests/Meetings/GetMeetingRecordCountRequest.cs
@@ -0,0 +1,12 @@
+using Mediator.Net.Contracts;
+using SugarTalk.Messages.Responses;
+
+namespace SugarTalk.Messages.Requests.Meetings;
+
+public class GetMeetingRecordCountRequest : IRequest
+{
+}
+
+public class GetMeetingRecordCountResponse : SugarTalkResponse<int>
+{
+}
