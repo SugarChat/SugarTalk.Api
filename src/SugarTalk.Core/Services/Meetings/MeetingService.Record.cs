2a6186604581ab38017fd0bbe69de081c87bdc37
JOHNNY.X <2546088435@qq.com>
2025-03-26 18:43:56 +0800

    update

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
@@ -62,6 +62,8 @@ public partial interface IMeetingService
     Task<GetMeetingRecordCountResponse> GetMeetingRecordCountAsync(GetMeetingRecordCountRequest request, CancellationToken cancellationToken);
     
     Task RetrySpeechMaticsJobAsync(CreateSpeechMaticsJobCommand command, CancellationToken cancellationToken);
+
+    Task<GetMeetingRecordEgressResponse> GetMeetingRecordEgressAsync(GetMeetingRecordEgressRequest request, CancellationToken cancellationToken);
 }
 
 public partial class MeetingService
@@ -484,7 +486,6 @@ public partial class MeetingService
         await _meetingDataProvider.PersistMeetingRecordAsync(meeting.Id, meetingRecordId, egressId, cancellationToken).ConfigureAwait(false);
         
         meeting.IsActiveRecord = true;
-        meeting.CurrentEgressId = egressId;
 
         await _meetingDataProvider.UpdateMeetingAsync(meeting, cancellationToken).ConfigureAwait(false);
     }
@@ -635,5 +636,16 @@ public partial class MeetingService
          };
          
          await _smartiesDataProvider.CreateSpeechMaticsRecordAsync(meetingSpeechMaticsRecord, cancellationToken: cancellationToken).ConfigureAwait(false);
+     }
+
+     public async Task<GetMeetingRecordEgressResponse> GetMeetingRecordEgressAsync(
+         GetMeetingRecordEgressRequest request, CancellationToken cancellationToken)
+     {
+         var records = await _meetingDataProvider.GetMeetingRecordsAsync(id: request.RecordId, cancellationToken).ConfigureAwait(false);
+         
+         return new GetMeetingRecordEgressResponse
+         {
+             Data = records.FirstOrDefault()?.EgressId
+         };
      }
 }
