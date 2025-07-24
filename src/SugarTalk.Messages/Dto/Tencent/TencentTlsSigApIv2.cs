be12777a3dc87150a415126677e6161909334207
FREYA.F <freya.fang@sjfood.com>
2025-07-24 14:29:37 +0800

    update codes

--- src/SugarTalk.Messages/Dto/Tencent/TencentTlsSigApIv2.cs
+++ src/SugarTalk.Messages/Dto/Tencent/TencentTlsSigApIv2.cs
@@ -10,20 +10,20 @@ namespace SugarTalk.Messages.Dto.Tencent;
 
 public class TencentTlsSigApIv2
 { 
-    private readonly int sdkappid;
-    private readonly string key;
+    private readonly int _sdkappid;
+    private readonly string _key;
 
     public TencentTlsSigApIv2(int sdkappid, string key)
     {
-        this.sdkappid = sdkappid;
-        this.key = key;
+        _sdkappid = sdkappid;
+        _key = key;
     }
 
     private static byte[] CompressBytes(byte[] sourceByte)
     {
-        MemoryStream inputStream = new MemoryStream(sourceByte);
-        Stream outStream = CompressStream(inputStream);
-        byte[] outPutByteArray = new byte[outStream.Length];
+        var inputStream = new MemoryStream(sourceByte);
+        var outStream = CompressStream(inputStream);
+        var outPutByteArray = new byte[outStream.Length];
         outStream.Position = 0;
         outStream.Read(outPutByteArray, 0, outPutByteArray.Length);
         return outPutByteArray;
@@ -31,8 +31,8 @@ public class TencentTlsSigApIv2
 
     private static Stream CompressStream(Stream sourceStream)
     {
-        MemoryStream streamOut = new MemoryStream();
-        ZOutputStream streamZOut = new ZOutputStream(streamOut, zlibConst.Z_DEFAULT_COMPRESSION);
+        var streamOut = new MemoryStream();
+        var streamZOut = new ZOutputStream(streamOut, zlibConst.Z_DEFAULT_COMPRESSION);
         CopyStream(sourceStream, streamZOut);
         streamZOut.finish();
         return streamOut;
@@ -40,7 +40,7 @@ public class TencentTlsSigApIv2
 
     public static void CopyStream(System.IO.Stream input, System.IO.Stream output)
     {
-        byte[] buffer = new byte[2000];
+        var buffer = new byte[2000];
         int len;
         while ((len = input.Read(buffer, 0, 2000)) > 0)
         {
@@ -51,19 +51,19 @@ public class TencentTlsSigApIv2
 
     private string HMACSHA256(string identifier, long currTime, int expire, string base64UserBuf, bool userBufEnabled)
     {
-        string rawContentToBeSigned = "TLS.identifier:" + identifier + "\n"
-             + "TLS.sdkappid:" + sdkappid + "\n"
-             + "TLS.time:" + currTime + "\n"
-             + "TLS.expire:" + expire + "\n";
+        var rawContentToBeSigned = "TLS.identifier:" + identifier + "\n"
+                                   + "TLS.sdkappid:" + _sdkappid + "\n"
+                                   + "TLS.time:" + currTime + "\n"
+                                   + "TLS.expire:" + expire + "\n";
         if (true == userBufEnabled)
         {
             rawContentToBeSigned += "TLS.userbuf:" + base64UserBuf + "\n";
         }
         using (HMACSHA256 hmac = new HMACSHA256())
         {
-            UTF8Encoding encoding = new UTF8Encoding();
-            Byte[] textBytes = encoding.GetBytes(rawContentToBeSigned);
-            Byte[] keyBytes = encoding.GetBytes(key);
+            var encoding = new UTF8Encoding();
+            var textBytes = encoding.GetBytes(rawContentToBeSigned);
+            var keyBytes = encoding.GetBytes(_key);
             Byte[] hashBytes;
             using (HMACSHA256 hash = new HMACSHA256(keyBytes))
                 hashBytes = hash.ComputeHash(textBytes);
@@ -73,19 +73,19 @@ public class TencentTlsSigApIv2
 
     private string GenSig(string identifier, int expire, byte[] userbuf, bool userBufEnabled)
     {
-        DateTime epoch = new DateTime(1970, 1, 1); // unix timestamp
-        Int64 currTime = (Int64)(DateTime.UtcNow - epoch).TotalMilliseconds / 1000;
+        var epoch = new DateTime(1970, 1, 1); 
+        var currTime = (Int64)(DateTime.UtcNow - epoch).TotalMilliseconds / 1000;
 
         string base64UserBuf;
         string jsonData;
         if (true == userBufEnabled)
         {
             base64UserBuf = Convert.ToBase64String(userbuf);
-            string base64sig = HMACSHA256(identifier, currTime, expire, base64UserBuf, userBufEnabled);
-            dynamic jsonObj = new JObject();
+            var base64sig = HMACSHA256(identifier, currTime, expire, base64UserBuf, userBufEnabled);
+            var jsonObj = new JObject();
             jsonObj["TLS.ver"] = "2.0";
             jsonObj["TLS.identifier"] = identifier;
-            jsonObj["TLS.sdkappid"] = sdkappid;
+            jsonObj["TLS.sdkappid"] = _sdkappid;
             jsonObj["TLS.expire"] = expire;
             jsonObj["TLS.time"] = currTime;
             jsonObj["TLS.sig"] = base64sig;
@@ -94,17 +94,17 @@ public class TencentTlsSigApIv2
         }
         else
         {
-            string base64sig = HMACSHA256(identifier, currTime, expire, "", false);
-            dynamic jsonObj = new JObject();
+            var base64sig = HMACSHA256(identifier, currTime, expire, "", false);
+            var jsonObj = new JObject();
             jsonObj["TLS.ver"] = "2.0";
             jsonObj["TLS.identifier"] = identifier;
-            jsonObj["TLS.sdkappid"] = sdkappid;
+            jsonObj["TLS.sdkappid"] = _sdkappid;
             jsonObj["TLS.expire"] = expire;
             jsonObj["TLS.time"] = currTime;
             jsonObj["TLS.sig"] = base64sig;
             jsonData = JsonConvert.SerializeObject(jsonObj);
         }
-        byte[] buffer = Encoding.UTF8.GetBytes(jsonData);
+        var buffer = Encoding.UTF8.GetBytes(jsonData);
         return Convert.ToBase64String(CompressBytes(buffer))
             .Replace('+', '*').Replace('/', '-').Replace('=', '_');
     }
@@ -112,10 +112,5 @@ public class TencentTlsSigApIv2
     public string GenSig(string identifier, int expire = 180 * 86400)
     {
         return GenSig(identifier, expire, null, false);
-    }
-
-    public string GenSigWithUserBuf(string identifier, int expire, byte[] userbuf)
-    {
-        return GenSig(identifier, expire, userbuf, true);
     }
 }
