0db26af19fcb81d75addef99be8f20fcf705cec9
MonesyH <monesy.huang@sjfood.com>
2025-08-20 14:37:41 +0800

    Switch tencent screen recording resolution

--- src/SugarTalk.Messages/Requests/Tencent/GetTencentCloudKeyRequest.cs
+++ src/SugarTalk.Messages/Requests/Tencent/GetTencentCloudKeyRequest.cs
@@ -1,5 +1,6 @@
 using System;
 using Mediator.Net.Contracts;
+using SugarTalk.Messages.Enums.Tencent;
 using SugarTalk.Messages.Responses;
 
 namespace SugarTalk.Messages.Requests.Tencent;
@@ -17,4 +18,6 @@ public class GetTencentCloudKeyResponseData
     public string AppId { get; set; }
     
     public string SDKSecretKey { get; set; }
+    
+    public ScreenRecordingResolution Resolution { get; set; }
 }
