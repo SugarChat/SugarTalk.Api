effa877bbe79fbdc4df675eac6a83edb48e6537b
FREYA.F <freya.fang@sjfood.com>
2025-08-28 18:51:21 +0800

    update code

--- src/SugarTalk.Messages/Commands/Tencent/CreateCloudRecordingCommand.cs
+++ src/SugarTalk.Messages/Commands/Tencent/CreateCloudRecordingCommand.cs
@@ -28,6 +28,8 @@ public class RecordParams
     public ulong? RecordMode { get; set; }
     
     public ulong? StreamType { get; set; }
+
+    public ulong? MaxIdleTime { get; set; }
     
     public SubscribeStreamUserIds SubscribeStreamUserIds { get; set; }
     
