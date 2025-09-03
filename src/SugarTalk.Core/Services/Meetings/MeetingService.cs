d9711d79204eaf354fce5977ee06b39ed3f8102b
JOHNNY.X <2546088435@qq.com>
2025-09-03 11:02:32 +0800

    Update MeetingService.cs

--- src/SugarTalk.Core/Services/Meetings/MeetingService.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.cs
@@ -860,12 +860,12 @@ namespace SugarTalk.Core.Services.Meetings
                 
                 meeting.UpdateUserSession(updateUserSession);
 
-                // meeting.MeetingTokenFromLiveKit = user.Issuer switch
-                // {
-                //     UserAccountIssuer.Guest => _liveKitServerUtilService.GenerateTokenForGuest(user.UserName, updateUserSession.GuestName, meeting.MeetingNumber),
-                //     UserAccountIssuer.Self or UserAccountIssuer.Wiltechs => _liveKitServerUtilService.GenerateTokenForJoinMeeting(user, meeting.MeetingNumber),
-                //     _ => throw new ArgumentOutOfRangeException()
-                // };
+                meeting.MeetingTokenFromLiveKit = user.Issuer switch
+                {
+                    UserAccountIssuer.Guest => _liveKitServerUtilService.GenerateTokenForGuest(user.UserName, updateUserSession.GuestName, meeting.MeetingNumber),
+                    UserAccountIssuer.Self or UserAccountIssuer.Wiltechs => _liveKitServerUtilService.GenerateTokenForJoinMeeting(user, meeting.MeetingNumber),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
 
                 return updateUserSession;
             }
