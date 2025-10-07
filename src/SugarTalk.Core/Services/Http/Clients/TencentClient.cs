84445c4c373ceb03dcaba18fa6ec7f88aeb000a2
JOHNNY.X <2546088435@qq.com>
2025-10-07 15:47:39 +0800

    Update TencentClient.cs

--- src/SugarTalk.Core/Services/Http/Clients/TencentClient.cs
+++ src/SugarTalk.Core/Services/Http/Clients/TencentClient.cs
@@ -202,7 +202,7 @@ public class TencentClient : ITencentClient
 
         Log.Information("Tencent Meeting Usage:{@resp}", resp);
 
-        var usageCount = resp.UsageList.Sum(x => Convert.ToDouble(x.UsageValue[3]) * 9 + Convert.ToDouble(x.UsageValue[0]) + Convert.ToDouble(x.UsageValue[1]));
+        var usageCount = resp.UsageList.Sum(x =>  Convert.ToDouble(x.UsageValue[0]) + Convert.ToDouble(x.UsageValue[1]) * 2 + Convert.ToDouble(x.UsageValue[3]) * 9);
         var percentage = usageCount/_tencentCloudSetting.TotalMonthlyUsage;
         var week = GetWeekOfMonth(request.CurrentDate);
         
