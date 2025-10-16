368db88af32bd303fb0527868dcb78cf8bd51327
JOHNNY.X <2546088435@qq.com>
2025-10-16 15:05:41 +0800

    Update MeetingService.Record.cs

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Record.cs
@@ -253,7 +253,7 @@ public partial class MeetingService
 
             if (stopResponse == null) throw new Exception();
         
-            var participants = await _meetingDataProvider.GetUserSessionsByMeetingIdAsync(command.MeetingId, record.MeetingSubId, true, true, true, cancellationToken).ConfigureAwait(false);
+            var participants = await _meetingDataProvider.GetUserSessionsByMeetingIdAsync(command.MeetingId, record.MeetingSubId, true, true, true, cancellationToken: cancellationToken).ConfigureAwait(false);
         
             var filterGuest = participants.Where(p => p.GuestName == null || p.IsEntryMeeting).ToList();
             Log.Information("filter guest response: {@filterGuest}", filterGuest);
