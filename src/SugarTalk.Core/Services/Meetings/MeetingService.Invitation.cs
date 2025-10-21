1aa2d052d2553d8db1acb3b3793774820ad19b37
JOHNNY.X <2546088435@qq.com>
2025-10-21 10:31:59 +0800

    update

--- src/SugarTalk.Core/Services/Meetings/MeetingService.Invitation.cs
+++ src/SugarTalk.Core/Services/Meetings/MeetingService.Invitation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Mediator.Net;
 using Serilog;
 using SugarTalk.Core.Domain.Meeting;
 using SugarTalk.Messages.Commands.Meetings;
@@ -16,17 +17,15 @@ namespace SugarTalk.Core.Services.Meetings;
 
 public partial interface IMeetingService
 {
-    //添加邀请人
     Task<CreateMeetingInvitationRecordsResponse> CreateMeetingInvitationRecordsAsync(CreateMeetingInvitationRecordsCommand command, CancellationToken cancellationToken);
     
-    //获取人员框架
     Task<GetMeetingInvitationUsersResponse> GetMeetingInvitationUsersAsync(GetMeetingInvitationUsersRequest request, CancellationToken cancellationToken);
     
-    //修改状态
     Task<UpdateMeetingInvitationRecordsResponse> UpdateMeetingInvitationRecordAsync(UpdateMeetingInvitationRecordsCommand command, CancellationToken cancellationToken);
     
-    //获取邀请信息
     Task<GetMeetingInvitationRecordsResponse> GetMeetingInvitationRecordsAsync(GetMeetingInvitationRecordsRequest request, CancellationToken cancellationToken);
+
+    Task GeneralMeetingInvitationRecordStatusAsync(GeneralMeetingInvitationRecordStatusCommand command, CancellationToken cancellationToken);
 }
 
 public partial class MeetingService : IMeetingService
@@ -53,6 +52,11 @@ public partial class MeetingService : IMeetingService
         Log.Information("Meeting invitation records: {@records}", records);
 
         await _meetingDataProvider.AddMeetingInvitationRecordsAsync(records, cancellationToken: cancellationToken).ConfigureAwait(false);
+        
+        _sugarTalkBackgroundJobClient.Schedule<IMediator>(m => m.SendAsync(new GeneralMeetingInvitationRecordStatusCommand
+        {
+            Ids = records.Select(x => x.Id).ToList()
+        }, cancellationToken), TimeSpan.FromSeconds(30));
 
         return new CreateMeetingInvitationRecordsResponse
         {
@@ -101,10 +105,7 @@ public partial class MeetingService : IMeetingService
             }
         }
     }
-
-    public async Task UpdateMeetingInvitationRecordAsync()
-    {
-    }
+    
 
     public async Task<GetMeetingInvitationRecordsResponse> GetMeetingInvitationRecordsAsync(GetMeetingInvitationRecordsRequest request, CancellationToken cancellationToken)
     {
@@ -141,5 +142,20 @@ public partial class MeetingService : IMeetingService
         {
             Data = _mapper.Map<List<MeetingInvitationRecordDto>>(meetingInvitationRecords)
         };
+    }
+
+    public async Task GeneralMeetingInvitationRecordStatusAsync(GeneralMeetingInvitationRecordStatusCommand command, CancellationToken cancellationToken)
+    {
+        var records = await _meetingDataProvider.GetMeetingInvitationRecordsAsync(command.Ids, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        records = records.Select(x =>
+        {
+            if (x.InvitationStatus == MeetingInvitationStatus.InvitationPending)
+                x.InvitationStatus = MeetingInvitationStatus.Invited;
+
+            return x;
+        }).ToList();
+
+        await _meetingDataProvider.UpdateMeetingInvitationRecordsAsync(records, cancellationToken: cancellationToken).ConfigureAwait(false);
     }
 }
